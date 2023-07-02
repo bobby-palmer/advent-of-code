ff76b8e538f84e5bb6a6e6f0dd0a9932add0c0e7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-02 11:00:04 -0400

    added caching to get_max and tested

--- 2022/day19/src/lib.rs
+++ 2022/day19/src/lib.rs
@@ -1,10 +1,4 @@
-use std::collections::BTreeMap;
-
-struct Cost {
-    ore: u8,
-    clay: u8,
-    obsidian: u8,
-}
+use std::collections::HashSet;
 enum Robot {
     Ore,
     Clay,
@@ -12,10 +6,17 @@ enum Robot {
     Geode,
 }
 struct Blueprint {
-    map: BTreeMap<Robot, Cost>,
+    ore: u8,
+    clay: u8,
+    obsidian: (u8, u8),
+    geode: (u8, u8),
 }
-
-#[derive(Clone)]
+impl Blueprint {
+    fn build(input: &str) -> Vec<Self> {
+        Vec::new()
+    }
+}
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct State {
     time: u8,
     orebots: u8,
@@ -28,6 +29,109 @@ struct State {
     geodes: u8,
 }
 impl State {
-    fn build(input: &str) -> Self {}
-    fn get_max(&self, blueprint: &Blueprint) -> u8 {}
+    fn new(time: u8) -> Self {
+        Self {
+            time,
+            orebots: 1,
+            claybots: 0,
+            obsidianbots: 0,
+            geodebots: 0,
+            ore: 0,
+            clay: 0,
+            obsidian: 0,
+            geodes: 0,
+        }
+    }
+    fn can_build(&self, bp: &Blueprint) -> Vec<Robot> {
+        let mut result = Vec::new();
+        if self.ore >= bp.ore {
+            result.push(Robot::Ore);
+        }
+        if self.ore >= bp.clay {
+            result.push(Robot::Clay);
+        }
+        {
+            let (ore, clay) = bp.obsidian;
+            if self.ore >= ore && self.clay >= clay {
+                result.push(Robot::Obsidian);
+            }
+        }
+        {
+            let (ore, obsidian) = bp.geode;
+            if self.ore >= ore && self.obsidian >= obsidian {
+                result.push(Robot::Geode);
+            }
+        }
+        result
+    }
+    fn build(mut self, robot: &Robot, bp: &Blueprint) -> Self {
+        match robot {
+            Robot::Ore => {
+                self.ore -= bp.ore;
+                self.orebots += 1;
+            }
+            Robot::Clay => {
+                self.ore -= bp.clay;
+                self.claybots += 1;
+            }
+            Robot::Obsidian => {
+                let (ore, clay) = bp.obsidian;
+                self.ore -= ore;
+                self.clay -= clay;
+                self.obsidianbots += 1;
+            }
+            Robot::Geode => {
+                let (ore, obsidian) = bp.geode;
+                self.ore -= ore;
+                self.obsidian -= obsidian;
+                self.geodebots += 1;
+            }
+        }
+        self
+    }
+    fn step(&self) -> Self {
+        let mut new = self.clone();
+        new.time -= 1;
+        new.ore += new.orebots;
+        new.clay += new.claybots;
+        new.obsidian += new.obsidianbots;
+        new.geodes += self.geodebots;
+        new
+    }
+    fn get_max(&self, bp: &Blueprint, cache: &mut HashSet<State>) -> u8 {
+        if self.time == 0 {
+            self.geodes
+        } else {
+            let mut result = 0;
+            if cache.insert(self.clone()) {
+                result = self.step().get_max(bp, cache);
+                result = std::cmp::max(
+                    result,
+                    self.can_build(bp)
+                        .iter()
+                        .map(|bot| self.step().build(bot, bp).get_max(bp, cache))
+                        .max()
+                        .unwrap_or_default(),
+                );
+            }
+            result
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn get_max() {
+        let bp = Blueprint {
+            ore: 4,
+            clay: 2,
+            obsidian: (3, 14),
+            geode: (2, 7),
+        };
+        let state = State::new(24);
+        let mut cache = HashSet::new();
+        assert_eq!(state.get_max(&bp, &mut cache), 9);
+    }
 }
