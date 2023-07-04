c32c9ca5c194fb1db1ea9ac9bffe6f964447f739
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-04 08:30:25 -0400

    solved part b

--- 2022/day21/src/lib.rs
+++ 2022/day21/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn get<'a>(input: &'a str, name: &str) -> &'a str {
     input
         .lines()
@@ -26,10 +28,76 @@ fn parse(input: &str, name: &str) -> i64 {
         }
     }
 }
+fn contains<'a>(input: &'static str, head: &'a str, name: &str, set: &mut HashSet<&'a str>) {
+    let line = get(input, head);
+    if name == head {
+        set.insert(head);
+    } else if line.parse::<i32>().is_ok() {
+        return;
+    } else {
+        let (lhs, rest) = line.split_once(' ').unwrap();
+        let (_, rhs) = rest.split_once(' ').unwrap();
+        contains(input, lhs, name, set);
+        contains(input, rhs, name, set);
+        if set.contains(lhs) || set.contains(rhs) {
+            set.insert(head);
+        }
+    }
+}
+fn reverse(input: &str, mut constant: i64, var_head: &str, var_name: &str, branch: &HashSet<&str>) -> i64 {
+    if var_head == var_name {
+        constant
+    } else {
+        let val = get(input, var_head);
+        let (lhs, rest) = val.split_once(' ').unwrap();
+        let (oper, rhs) = rest.split_once(' ').unwrap();
+        let var_head;
+        if branch.contains(lhs) {
+            var_head = lhs;
+            let value = parse(input, rhs);
+            constant = match oper {
+                "*" => constant / value,
+                "+" => constant - value,
+                "-" => constant + value,
+                "/" => constant * value,
+                _ => unreachable!(),
+            };
+        } else {
+            var_head = rhs;
+            let value = parse(input, lhs);
+            constant = match oper {
+                "*" => constant / value,
+                "+" => constant - value,
+                "-" => value - constant,
+                "/" => value / constant,
+                _ => unreachable!(),
+            };
+        }
+        reverse(input, constant, var_head, var_name, branch)
+    }
+}
 pub fn solve() {
     let input = include_str!("../input");
     let solution = parse(input, "root");
     println!("{solution}");
+    let solution_b = solve_b(input);
+    println!("found solution b : {solution_b}");
+
+}
+pub fn solve_b(input: &'static str) -> i64 {
+    let line = get(input, "root");
+    let (lhs, rest) = line.split_once(' ').unwrap();
+    let (_, rhs) = rest.split_once(' ').unwrap();
+    let mut set = HashSet::new();
+    contains(input, "root", "humn", &mut set);
+    let result = if set.contains(lhs) {
+        let constant = parse(input, rhs);
+        reverse(input, constant, lhs, "humn", &set)
+    } else {
+        let constant = parse(input, lhs);
+        reverse(input, constant, rhs, "humn", &set)
+    };
+    result
 }
 #[cfg(test)]
 mod test {
@@ -61,6 +129,24 @@ mod test {
         let input = include_str!("../testinput");
         let solution = parse(input, "root");
         assert_eq!(solution, 152);
+    }
+    #[test]
+    fn get_containing() {
+        let input = include_str!("../testinput");
+        let mut set = HashSet::new();
+        contains(input, "root", "humn", &mut set);
+        assert!(set.contains("humn"));
+        assert!(set.contains("ptdq"));
+        assert!(set.contains("lgvd"));
+        assert!(set.contains("cczh"));
+        assert!(set.contains("pppw"));
+        assert!(set.contains("root"));
+        assert_eq!(set.len(),6);
+    }
+    #[test]
+    fn solve_pt2() {
+        let input = include_str!("../testinput");
+        assert_eq!(solve_b(input),301);
     }
 }
 
