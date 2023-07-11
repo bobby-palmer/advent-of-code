72d80a9753a731b5110a46448e00074ecfaa9a71
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-07-11 16:19:14 -0400

    implemented a surroundings check

--- 2022/day23/src/lib.rs
+++ 2022/day23/src/lib.rs
@@ -1,5 +1,8 @@
 use std::collections::BTreeSet;
 
+const INPUT: &str = include_str!("../input");
+const TEST: &str = include_str!("../testinput");
+
 #[derive(PartialEq, PartialOrd, Eq, Ord)]
 struct Elf {
     row: i64,
@@ -18,16 +21,40 @@ fn parse(input: &str) -> BTreeSet<Elf> {
         })
     }).flatten().collect()
 }
+fn surrounded(elf: &Elf, elves: &BTreeSet<Elf>) -> bool {
+    for row in -1..=1 {
+        for col in -1..=1 {
+            if !(row == 0 && col == 0) && elves.contains(&Elf {
+                row: elf.row + row,
+                col: elf.col + col,
+            }) {
+                return true
+            }
+        }
+    }
+    false
+}
+fn simulate_round(input: &mut BTreeSet<Elf>)  {
+    unimplemented!();
+}
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test_parse() {
-        let input = include_str!("../testinput");
-        let tree = parse(input);
+        let tree = parse(TEST);
         assert!(tree.contains(&Elf {
             row: 1,
             col: 2,
         }));
+    }
+    #[test]
+    fn test_surroundings() {
+       let tree = parse(TEST);
+        let test_elf = &Elf {
+            row:1,
+            col:2,
+        };
+        assert!(surrounded(test_elf, &tree));
     }
 }
