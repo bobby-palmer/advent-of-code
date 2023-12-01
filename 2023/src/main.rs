3bfa65085c3bbff4ddc38350fb90857fdbc90a61
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-12-01 12:55:36 -0500

    Impl runner function

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,6 +1,16 @@
+use std::io;
+
 mod one;
 
 fn main() -> std::io::Result<()> {
-    one::solve("day1.txt")?;
+    let mut input = String::new();
+    let stdin = io::stdin();
+    println!("Which day do you want to run? (ie. 'one' will solve day one, part a and b)");
+    stdin.read_line(&mut input)?;
+
+    match input.trim() {
+        "one" => one::solve("day1.txt")?,
+        x => println!("{x} is not implemented yet!"),
+    }
     Ok(())
 }
