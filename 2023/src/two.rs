767572519ee6e2198fa93ef15eda1ec34a33e270
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-12-02 00:34:37 -0500

    Add testing

--- 2023/src/two.rs
+++ 2023/src/two.rs
@@ -49,3 +49,25 @@ fn solve_b(s: &str) -> u32 {
         r * g * b
     }).sum()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const S: &str = 
+"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    #[test]
+    fn test_a() {
+       assert_eq!(solve_a(S), 8); 
+    }
+
+    #[test]
+    fn test_b() {
+        assert_eq!(solve_b(S), 2286);
+    }
+}
