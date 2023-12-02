fdfc2467243d513f8ace05d5bdf3772a5a34d417
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-12-02 09:12:53 -0500

    Make more modular

--- 2023/src/two.rs
+++ 2023/src/two.rs
@@ -1,4 +1,6 @@
 use crate::util;
+use std::collections::HashMap;
+
 
 pub fn solve() -> std::io::Result<()> {
     let input = util::read("day2.txt")?;
@@ -9,45 +11,45 @@ pub fn solve() -> std::io::Result<()> {
     Ok(())
 }
 
-fn solve_a(s: &str) -> u32 {
-    s.lines().enumerate().map(|(i, l)| {
-        let mut it = l.split_whitespace();
-        it.next();
-        it.next();
-        while let Some(num) = it.next() {
-            let color = it.next().unwrap();
-            let n = num.parse::<i32>().unwrap();
-            if color.contains("red") && n > 12 {
-               return 0u32 
-            } else if color.contains("green") && n > 13 {
-                return 0u32
-            } else if color.contains("blue") && n > 14 {
-                return 0u32
-            }
-        }
-        i as u32 + 1
-    }).sum()
+fn map_a(t: (usize, &str)) -> i32 {
+    let maxes: HashMap<char, i32> = HashMap::from([
+        ('r', 12),
+        ('g', 13),
+        ('b', 14),
+    ]);
+
+   let (i, l) = t; 
+   let mut it = l.split_whitespace();
+   it.nth(1); // skip first two
+
+   while let (Some(n), Some(c)) = (it.next(), it.next()) {
+       let num = n.parse::<i32>().unwrap(); 
+       if maxes[&c.chars().next().unwrap()] < num {
+           return 0
+       }
+   }
+   i as i32 + 1
+}
+fn solve_a(s: &str) -> i32 {
+    util::sum_map(s, map_a)
 }
 
-fn solve_b(s: &str) -> u32 {
-    s.lines().map(|l| {
-        let mut it = l.split_whitespace();
-        it.next();
-        it.next();
-        let (mut r, mut g, mut b) = (0, 0, 0);
-        while let Some(num) = it.next() {
-            let c = it.next().unwrap();
-            let v = num.parse().unwrap();
-            if c.contains("red") {
-                r = std::cmp::max(r, v);
-            } else if c.contains("blue") {
-                b = std::cmp::max(b, v);
-            } else {
-                g = std::cmp::max(g, v);
-            }
+fn map_b(t: (usize, &str)) -> i32 {
+    let (_, l) = t;
+    let mut it = l.split_whitespace();
+    let (mut r, mut g, mut b) = (0, 0, 0);
+    while let (Some(n), Some(c)) = (it.next(), it.next()) {
+        match c.chars().next() {
+            Some('r') => r = std::cmp::max(r, n.parse().unwrap()),
+            Some('g') => g = std::cmp::max(g, n.parse().unwrap()),
+            Some('b') => b = std::cmp::max(b, n.parse().unwrap()),
+            _ => (),
         }
-        r * g * b
-    }).sum()
+    }
+    r * g * b
+}
+fn solve_b(s: &str) -> i32 {
+    util::sum_map(s, map_b)
 }
 
 #[cfg(test)]
--- 2023/src/util.rs
+++ 2023/src/util.rs
@@ -3,3 +3,11 @@ pub fn read(file: &str) -> io::Result<String> {
     let s = std::fs::read_to_string(file)?;
     Ok(s)
 }
+
+/**
+ *  Takes a function and applies it to each line of the input.
+ *  returning the sum of all of these mappings as an i32.
+ */
+pub fn sum_map(s: &str, f: fn((usize, &str)) -> i32) -> i32 {
+    s.lines().enumerate().map(f).sum()
+}
